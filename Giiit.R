37ce79538f790bddbf30ab15feb12df73539194d
Sheila Wachiye <sawachiye@gmail.com>
2023-07-24 09:46:37 +0300

    Save

--- Giiit.R
+++ Giiit.R
@@ -16,5 +16,38 @@ head(data)
 #To do so, we can use the pairs() function to create a scatterplot 
 #of every possible pair of variables:
   
-  pairs(data, pch = 18, col = "steelblue")
+pairs(data, pch = 18, col = "steelblue")
+#From this pairs plot we can see the following:
+#mpg and disp appear to have a strong negative linear correlation
+#mpg and hp appear to have a strong positive linear correlation
+#mpg and drat appear to have a modest negative linear correlation
   
+# We we could also use the ggpairs() function from the GGally library 
+#install and load the GGally library
+install.packages("GGally")
+library(GGally)
+
+#generate the pairs plot
+ggpairs(data)
+
+#Fitting the Model
+#The basic syntax to fit a multiple linear regression model in R is as follows:
+#lm(response_variable ~ predictor_variable1 + predictor_variable2 + ..., data = data)  
+model <- lm(mpg ~ disp + hp + drat, data = data)
+summary(model)
+
+#Checking Assumptions of the Model
+#1. The distribution of model residuals should be approximately normal.
+#We can check if this assumption is met by creating a simple histogram of residuals:
+hist(residuals(model), col = "steelblue")
+#Although the distribution is slightly right skewed,
+#it isn’t abnormal enough to cause any major concerns.
+#2. The variance of the residuals should be consistent for all observations.
+#This preferred condition is known as homoskedasticity. 
+#Violation of this assumption is known as heteroskedasticity.
+#To check if this assumption is met we can create a fitted value vs. residual plot:
+#create fitted value vs residual plot
+plot(fitted(model), residuals(model))
+
+#add horizontal line at 0
+abline(h = 0, lty = 2)
