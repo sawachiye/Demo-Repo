23757425becc113607d7af2a990174defd06d6ae
Sheila Wachiye <sawachiye@gmail.com>
2023-07-24 10:44:09 +0300

    Done

---
+++ ExponentialRegression.R
@@ -0,0 +1,23 @@
+## Exponential Regression in R (Step-by-Step)
+#Exponential regression is a type of regression that can be used to model the following situations:
+  
+  #1. Exponential growth: Growth begins slowly and then accelerates rapidly without bound.
+  #2. Exponential decay: Decay begins rapidly and then slows down to get closer and closer to zero.
+ #y = ab^x   where:
+    #y: The response variable
+    #x: The predictor variable
+    #a, b: The regression coefficients that describe the relationship between x and y
+#Create the Data
+#create some fake data for two variables: x and y:
+  
+x = 1:20
+y = c(1, 3, 5, 7, 9, 12, 15, 19, 23, 28, 33, 38, 44, 50, 56, 64, 73, 84, 97, 113)
+
+#Visualize the Data
+plot(x, y)
+
+#fit the model
+model <- lm(log(y)~ x)
+
+#view the output of the model
+summary(model)
